51b8683266a732858bf447c6abcdebe364b1aeb1
Lautaro López <lautaro@hotmail.com>
2021-07-05 11:08:08 -0300

    Added  parameter

--- src/Traits/Cartable.php
+++ src/Traits/Cartable.php
@@ -13,10 +13,10 @@ trait Cartable
      * @param int quantity
      * @return
      */
-    public static function addToCart($id, $quantity = 1)
+    public static function addToCart($id, $quantity = 1, $customAttributes = [])
     {
         $class = static::class;
 
-        return app(Cart::class)->add($class::findOrFail($id), $quantity);
+        return app(Cart::class)->add($class::findOrFail($id), $quantity, $customAttributes);
     }
 }
